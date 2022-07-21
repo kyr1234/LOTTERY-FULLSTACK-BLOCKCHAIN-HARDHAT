d973b5e80883bb8402e1775480123120d4faf9fc
kyr1234 <yugkhokhar18@gmail.com>
2022-07-21 19:06:04 +0530

    Added Raffle state

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -8,17 +8,25 @@ import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 import "hardhat/console.sol";
 
 error Raffle__SendMoreToEnterRaffle();
+error Raffle_FundNotTransfer();
+error Raffle_NotOpen();
 
-contract Raffle is VRFConsumerBaseV2 {
+contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+
     event RaffleEnter(address indexed player);
     event RequestedRandomWinner(uint256 indexed requestid);
-    
-
-
-
+    event WinnerPicked(address indexed winner);
 
     uint256 private i_entranceFee;
     address payable[] private s_players;
+    address private recent_winner;
+
+    RaffleState private s_rafflestate;
+
     VRFCoordinatorV2Interface private immutable i_vrfcoordinator;
     bytes32 private immutable gaslane;
     uint64 private immutable i_subid;
@@ -38,6 +46,7 @@ contract Raffle is VRFConsumerBaseV2 {
         gaslane = gasLane;
         i_subid = subid;
         i_callbackGasLimit = callbackGasLimit;
+        s_rafflestate = RaffleState(0);
     }
 
     function enterRaffle() public payable {
@@ -46,12 +55,21 @@ contract Raffle is VRFConsumerBaseV2 {
         if (msg.value < i_entranceFee) {
             revert Raffle__SendMoreToEnterRaffle();
         }
+        if (s_rafflestate != RaffleState(0)) {
+            revert Raffle_NotOpen();
+        }
 
         s_players.push(payable(msg.sender));
         // Emit an event when we update a dynamic array or mapping
         // Named events with the function name reversed
         emit RaffleEnter(msg.sender);
     }
+
+    function checkUp(bytes calldata checkdata)
+        external
+        override
+        returns (bool state)
+    {}
 
     function requestrandomWinner() external {
         uint256 requestid = i_vrfcoordinator.requestRandomWords(
@@ -65,20 +83,20 @@ contract Raffle is VRFConsumerBaseV2 {
         emit RequestedRandomWinner(requestid);
     }
 
-    /**
-     * @dev This is the function that Chainlink VRF node
-     * calls to send the money to the random winner.
-     */
     function fulfillRandomWords(
-        uint256, /* requestId */
+        uint256,
+        /*requestId*/
         uint256[] memory randomWords
     ) internal override {
-        // s_players size 10
-        // randomNumber 202
-        // 202 % 10 ? what's doesn't divide evenly into 202?
-        // 20 * 10 = 200
-        // 2
-        // 202 % 10 = 2
+        uint256 Winnerindex = randomWords[0] % s_players.length;
+        address payable recentwinner = s_players[Winnerindex];
+        (bool success, ) = recentwinner.call{value: address(this).balance}("");
+        recent_winner = recentwinner;
+        if (!success) {
+            revert Raffle_FundNotTransfer();
+        }
+
+        emit WinnerPicked(recentwinner);
     }
 
     /** Getter Functions */
@@ -93,5 +111,9 @@ contract Raffle is VRFConsumerBaseV2 {
 
     function getNumberOfPlayers() public view returns (uint256) {
         return s_players.length;
+    }
+
+    function getRecentWinner() public view returns (address) {
+        return recent_winner;
     }
 }
